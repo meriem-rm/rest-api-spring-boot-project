39b948f9c362c89776924394c7abc83c72779396
meriem <meriem.ramli@benchmatrix.ca>
2022-02-14 07:57:36 -0800

    firstcommit

--- src/main/java/com/example/demo/location/LocationController.java
+++ src/main/java/com/example/demo/location/LocationController.java
@@ -4,41 +4,43 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LocationController {
 	
 	@Autowired
 	private LocationService locationService;
      
-	 @RequestMapping(value="/location")
-	 List<Location> getLocation() {
-		return locationService.getAllLocation();
-		 
-	 } 
+	@RequestMapping(value = "/locations")
+	 public String getAllLocations(Model model) 
+	 {				
+		model.addAttribute("locations", locationService.getAllLocation());		
+		return "location";		
+	 }
 	 
-	 @RequestMapping(value="/location/{id}")
+	 @RequestMapping(value="/locations/{id}")
 	 public Optional<Location> getLocation(@PathVariable String id ) {
 		 return locationService.getLocation(id);
 	 }
 	
-	 @RequestMapping(value="/location", method=RequestMethod.POST)
+	 @RequestMapping(value="/locations", method=RequestMethod.POST)
 	 public void addLocation(@RequestBody Location location)  {
 		 locationService.addLocation(location);
 	 }
 	 
-	 @RequestMapping(value="/location/{id}", method = RequestMethod.PUT)
+	 @RequestMapping(value="/locations/{id}", method = RequestMethod.PUT)
 	 public void updateLocation(@RequestBody Location location, @PathVariable String id) {
 
 	      locationService.updateLocation(id, location);
 	 } 
 	 
-	 @RequestMapping(value="/location/{id}", method=RequestMethod.DELETE)	 
+	 @RequestMapping(value="/locations/{id}", method=RequestMethod.DELETE)	 
 	 public void deleteLocation(@PathVariable String id ) {
 		 locationService.deleteLocation(id);
 	 }
